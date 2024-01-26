efe3bc17471f7e07e352314edad3511c658d132e
Pranj99 <pranjali.barve2499@gmail.com>
2024-01-26 12:09:34 -0800

    challenge 5
    fixed sensor based object destruction

--- Assets/Challenge 5/Scripts/TargetX.cs
+++ Assets/Challenge 5/Scripts/TargetX.cs
@@ -26,8 +26,9 @@ public class TargetX : MonoBehaviour
 
     }
 
+
     // When target is clicked, destroy it, update score, and generate explosion
-    private void OnMouseEnter()
+    private void OnMouseDown()
     {
         if (gameManagerX.isGameActive)
         {
